a75d2ddb2a1c01c461e1abd9974faaa0332424b6
ben <30186471+Gammage@users.noreply.github.com>
2026-04-30 02:42:01 +0100

    finished modules, ready for lapotop test

--- modules/core.nix
+++ modules/core.nix
@@ -1,8 +1,25 @@
 {
   flake.modules.nixos.core = { hostname, ... }: {
-
+    
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+    
+    # Audio (PipeWire)
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
 
+    # Bluetooth
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    services.blueman.enable = true;
+    
     services.xserver = {
         enable = true;
         displayManager.lightdm.enable = true;
