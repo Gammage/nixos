39e674e24afe1cd06ebde17e8c8db993ffd2e38b
ben <30186471+Gammage@users.noreply.github.com>
2026-05-03 19:47:46 +0100

    added opencode pkg

--- modules/programs/opencode.nix
+++ modules/programs/opencode.nix
@@ -1,7 +1,11 @@
-{ flake.modules.homeManager.opencode = { ... }: {
+{ flake.modules.homeManager.opencode = { pkgs, ... }: {
   home.file.".opencode".source = ./opencode-config;
 
   home.sessionVariables = {
     OPENCODE_CONFIG_DIR = "$HOME/.opencode";
   };
+
+  home.packages = with pkgs; [
+    opencode
+  ];
 }; }
