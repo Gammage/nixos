8801929c68506d84062776f83ccbb996f355984f
Gammage <30186471+Gammage@users.noreply.github.com>
2026-05-05 17:23:25 +0100

    added syntax without dollar

--- modules/programs/i3.nix
+++ modules/programs/i3.nix
@@ -1,13 +1,12 @@
 { flake.modules.homeManager.i3 = { pkgs, ... }: {
   home.packages = with pkgs; [ dmenu i3 i3status ];
   home.file.".config/i3/config".source = pkgs.writeText "i3-config" ''
-    set $mod Mod1
     terminal wezterm
 
-    bindsym $mod+d exec dmenu_run
-    bindsym $mod+Return exec wezterm
-    bindsym $mod+Shift+q kill
-    bindsym $mod+Shift+e exec i3-msg exit
+    bindsym Mod1+d exec dmenu_run
+    bindsym Mod1+Return exec wezterm
+    bindsym Mod1+Shift+q kill
+    bindsym Mod1+Shift+e exec i3-msg exit
 
     bar {
       status_command ${pkgs.i3status}/bin/i3status
