420c08ae53a3f20e22cfe853db3bd19d8fcfb95b
ben <30186471+Gammage@users.noreply.github.com>
2026-05-06 03:47:05 +0100

    bleb

--- modules/programs/i3.nix
+++ modules/programs/i3.nix
@@ -3,5 +3,8 @@
   home.file.".config/i3".source = ./i3-config;
 
   # Script to open WezTerm with tmux in ~/notes and launch nvim
-  home.file.".local/bin/notes_station".source = ./i3-config/notes_station.sh;
+  home.file.".local/bin/notes_station" = {
+    source = ./i3-config/notes_station.sh;
+    executable = true;
+  };
 }; }
