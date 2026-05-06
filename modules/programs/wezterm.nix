b54d07ac502c063002ca40b0d885d78a9761fea3
ben <30186471+Gammage@users.noreply.github.com>
2026-05-06 03:20:08 +0100

    added work workspace

--- modules/programs/wezterm.nix
+++ modules/programs/wezterm.nix
@@ -10,7 +10,8 @@
       	color_scheme = "Afterglow (Gogh)",
       	hide_tab_bar_if_only_one_tab = true,
       	use_fancy_tab_bar = false,
-      	font = wezterm.font("Hurmit Nerd Font"),
+      font = wezterm.font("Hurmit Nerd Font"),
+        default_prog = { "tmux", "new-session", "-A", "-s", "main" },
       }
 
       return config
