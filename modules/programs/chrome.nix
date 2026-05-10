aafedb08e917d0d999fd15e429b6f4fb51a562d1
ben <30186471+Gammage@users.noreply.github.com>
2026-05-10 22:21:14 +0100

    added window fix

--- modules/programs/chrome.nix
+++ modules/programs/chrome.nix
@@ -1,7 +1,7 @@
 { flake.modules.homeManager.chrome = { pkgs, ... }: {
   home.packages = with pkgs; [
     (writeShellScriptBin "chrome" ''
-      exec ${google-chrome}/bin/google-chrome-stable --new-window "$@"
+      exec google-chrome-stable --new-window "$@"
     '')
   ];
 }; }
