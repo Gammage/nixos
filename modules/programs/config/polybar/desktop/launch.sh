1eaee5b0002b0ca48c3ca66fb1332fd248e8bdd7
ben <30186471+Gammage@users.noreply.github.com>
2026-06-06 22:33:12 +0100

    bar

--- modules/programs/config/polybar/desktop/launch.sh
+++ modules/programs/config/polybar/desktop/launch.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    if xrandr --query | grep -A 10 "^$m connected" | grep -q "\*"; then
+        MONITOR=$m polybar --reload main &
+    fi
 done
