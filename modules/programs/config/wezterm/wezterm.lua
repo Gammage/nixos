4e31410128279fb704ec355a29c07a8f95b5ac81
ben <30186471+Gammage@users.noreply.github.com>
2026-06-22 23:35:02 +0100

    fixed tmuxconf

--- modules/programs/config/wezterm/wezterm.lua
+++ modules/programs/config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {
-    copy_on_select = true,
     window_decorations = "RESIZE",
     audible_bell = "Disabled",
     color_scheme = "Afterglow (Gogh)",
